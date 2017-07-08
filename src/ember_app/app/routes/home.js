b4381ed4a2a92d23baadd03580d7c3a59ac84f04
phi- <phi.liney@gmail.com>
2017-07-08 03:41:21 -0700

    removed controller

--- src/ember_app/app/routes/home.js
+++ src/ember_app/app/routes/home.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  code: null,
+
+  actions: {
+    sumbitCode() {
+      alert(this.get('code'));
+    },
+
+    updateCode(code) {
+      this.set('code', code);
+    }
+  }
 });
