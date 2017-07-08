d8aab148e5b50f102e5e3446a61cc237cb77bae5
phi- <phi.liney@gmail.com>
2017-07-08 03:26:20 -0700

    actually satisfied merge

--- src/ember_app/app/components/code-box.js
+++ src/ember_app/app/components/code-box.js
@@ -3,11 +3,7 @@ import config from '../config/environment';
 const {Component} = Ember;
 
 export default Component.extend({
-<<<<<<< HEAD
-
-=======
   codePlaceholder: config.classStruct,
-<<<<<<< HEAD
 
   actions: {
     textChanged(event) {
@@ -15,7 +11,4 @@ export default Component.extend({
       this.sendAction('action', text);
     }
   }
-=======
->>>>>>> ae69555b658c6254a2c1df407f7f7a12816805ab
->>>>>>> 66f24ca80eec11c86976b83f7785b0fb7ef756d9
 });
