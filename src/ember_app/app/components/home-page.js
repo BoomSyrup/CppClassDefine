3c21f81e1be92c37be2d0bcc7a4f9510e90d015f
WasianWay <byron4white@gmail.com.com>
2017-07-09 22:07:17 -0700

    Trying and failing at inplementing Regex

--- src/ember_app/app/components/home-page.js
+++ src/ember_app/app/components/home-page.js
@@ -7,17 +7,10 @@ export default Component.extend({
   actions: {
     sumbitCode() {
       var codeText = this.get('code');
-      codeText = codeText.replace(/\s\s+/g, " ");
-      var codeArray = codeText.split(" ");
-      codeArray.forEach(function(element)
-      {
-        console.log(element);
-      });
-      var classIndex = codeArray.indexOf("class");
-      var className = codeArray[classIndex + 1];
-      className = className.replace(/[^0-9a-z_-]/gi, '');
-      alert(className);
       var regexFunction = /([a-z_][a-z0-9_]*)+[\s]+([a-z_][a-z0-9_]*)+[()]+[;]/gi;
+      var regexClassforName = /class\s*([a-z_]\w*)/gi;
+      var regexClassTotal = /class\s*([a-z_]\w*)\s*{(.*)}/gi;
+
 
     },
 
