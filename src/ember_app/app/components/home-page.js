8ae6864b46aeef75545aa1b85bf9c0a9d6e1b72d
WasianWay <byron4white@gmail.com.com>
2017-07-10 00:08:26 -0700

    Josh said it was robust

--- src/ember_app/app/components/home-page.js
+++ src/ember_app/app/components/home-page.js
@@ -6,11 +6,14 @@ export default Component.extend({
 
   actions: {
     sumbitCode() {
+      var classDictionary = {};
+      var toParseArray = [];
       var codeText = this.get('code');
+
       var regexFunction = /([a-z_]\w*)\s([a-z_]\w*)((.*?));/gi;
       //var regexClassforName = /class\s*([a-z_]\w*)/gi;
-      var regexClassTotal = /class\s*([a-z_]\w*)\s*{([^}]*?)};/gi;
-      var toParseArray = [];
+      var regexClassTotal = /class\s*([a-z_]\w*)\s*{([\s\S]*?)};/gim;
+
       var execClass = regexClassTotal.exec(codeText);
       while(execClass!=null)
       {
@@ -18,9 +21,15 @@ export default Component.extend({
         execClass = regexClassTotal.exec(codeText);
       }
       toParseArray.forEach(function(classElement){
-        console.log(classElement);
+        console.log(classElement[2]);
+        var functionArray = [];
+        var execFunction = regexFunction.exec(classElement[2]);
+        console.log(execFunction);
+        //while(execFunction != null)
+        //{
+        //   //console.log(execFunction);
+        // }
       });
-      var classDictionary = {};
     },
 
     updateCode(code) {
